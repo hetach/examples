19dd8e9d438671473b465d95091f4fdfda1a940b
Gracjan Orzechowski <gracjan.orzechowski@rst.com.pl>
2017-01-22 01:42:17 +0100

    added route with param

--- simple/main.cpp
+++ simple/main.cpp
@@ -2,16 +2,23 @@
 #include "server/webserver.h"
 #include "http-kernel/controller.h"
 #include "http/response.h"
+#include "router/params.h"
+#include "router/paramnotfoundexception.h"
 
 using namespace Hetach;
 using namespace Hetach::HttpKernel;
 using namespace Hetach::Server;
+using namespace Hetach::Router;
 
 class IndexController: public Controller
 {
     void doGet()
     {
-        this->response()->setContent("Index controller");
+        try {
+            this->response()->setContent("Index controller " + this->routeParams()->value("someParam"));
+        } catch(ParamNotFoundException) {
+            this->response()->setContent("Index controller");
+        }
     }
 };
 
@@ -22,6 +29,7 @@ int main()
     Controller *controller = new IndexController();
 
     app->route("/index", controller);
+    app->route("/index/{someParam}", controller);
 
     return app->exec();
 }
