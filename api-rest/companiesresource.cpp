3049c82620bc93a680ec3acee8eb328a9dd16178
Gracjan Orzechowski <gracjan.orzechowski@rst.com.pl>
2017-03-04 00:59:41 +0100

    entity not found and api error

--- api-rest/companiesresource.cpp
+++ api-rest/companiesresource.cpp
@@ -3,6 +3,9 @@
 #include "companiesresource.h"
 #include "companyentity.h"
 
+#include "hetach/api-rest/entitynotfoundexception.h"
+#include "hetach/api-rest/exception.h"
+
 using namespace std;
 using namespace Hetach::ApiRest;
 
@@ -13,6 +16,12 @@ CompaniesResource::CompaniesResource(): Resource()
 
 Entity* CompaniesResource::fetch(int id)
 {
+    if(id == 0) {
+        throw EntityNotFoundException();
+    } else if(id < 0) {
+        throw Exception("Internal server error");
+    }
+
     return new CompanyEntity(id);
 }
 
